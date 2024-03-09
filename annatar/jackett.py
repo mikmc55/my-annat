a39f818d8f5cd6561e6a42b7772425249d7ee815
mick-25 <158113781+mick-25@users.noreply.github.com>
2024-03-09 00:52:59 +0000

    Update jackett.py

--- annatar/jackett.py
+++ annatar/jackett.py
@@ -24,8 +24,8 @@ from annatar.torrent import Category
 log = structlog.get_logger(__name__)
 
 
-JACKETT_URL: str = os.environ.get("JACKETT_URL", "http://localhost:9117")
-JACKETT_API_KEY: str = os.environ.get("JACKETT_API_KEY", "")
+JACKETT_URL: str = os.environ.get("JACKETT_URL", "http://167.86.78.215:9117")
+JACKETT_API_KEY: str = os.environ.get("JACKETT_API_KEY", "a6q3edvwsd4pl9155lyu0lrwmngq4awu")
 
 JACKETT_MAX_RESULTS = int(os.environ.get("JACKETT_MAX_RESULTS", 100))
 JACKETT_TIMEOUT = int(os.environ.get("JACKETT_TIMEOUT", 6))
@@ -35,7 +35,7 @@ JACKETT_CACHE_MINUTES = timedelta(minutes=int(os.environ.get("JACKETT_CACHE_MINU
 
 JACKETT_INDEXERS_LIST: list[str] = os.environ.get(
     "JACKETT_INDEXERS",
-    "yts,eztv,kickasstorrents-ws,thepiratebay,therarbg,torrentgalaxy,bitsearch,limetorrents,badasstorrents",
+    "aftershock,beload,bestcore,estone,filelist,itorrent,mypornclub,speedapp,thepiratebay,therarbg,ztracke",
 ).split(",")
 
 
